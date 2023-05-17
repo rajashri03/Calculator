6910202c9ea38494e8272eba12bc5f7c71ccc72d
rajashri03 <telvekar.rajashri@gmail.com>
2023-05-17 08:18:42 +0530

    [Rajashri]-adding addition part

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 10, b = 20, result;
+
+            //Addition
+            result = a + b;
+            Console.WriteLine(result);
+        }
+    }
+}
